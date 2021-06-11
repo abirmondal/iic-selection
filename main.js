d94d995055d8b87235b6253a2ff0d4b88cfa7328
Abir Mondal <84134625+abirmondal@users.noreply.github.com>
2021-06-11 18:28:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
 	
 	$('#menu').click(function(){
-		$('.right').slideToggle("slow");
+		var toggleleft = parseInt($('.right').css('left')) == -1000 ? 0 : -1000;
+		var fadebody = ($('#bodycon').css('opacity')) == 1 ? 0.1 : 1;
+		var scrollbody = ($('body').css('overflow')) == 'visible' ? 'hidden' : 'visible';
+		$('.right').animate({
+			left: toggleleft
+		}, 'slow');
+		$('#bodycon').fadeTo('slow', fadebody);
+		$('body').css('overflow', scrollbody);
 	});
 	$('#searchbtn').click(function(){
 		var togglewidth = $('#search').width() == 150 ? 0 : 150;
